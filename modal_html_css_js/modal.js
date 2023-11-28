8a56819df51a6913635cdb15efa480d581073ab8
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-11-27 17:43:45 -0700

    final push, modal project complete

--- modal_html_css_js/modal.js
+++ modal_html_css_js/modal.js
@@ -26,15 +26,6 @@ document.addEventListener("click", event => {
   }
 });
 
-// Closes Modal using ESC key
-// document.addEventListener("keydown", event => {
-//   if (event.key === "Escape") {
-//     if (!modal.classList.contains("hidden")) {
-//       closeModal();
-//     }
-//   }
-// });
-
 // More concise code 
 document.addEventListener("keydown", event => {
   if (event.key === "Escape" && !modal.classList.contains("hidden")) {
